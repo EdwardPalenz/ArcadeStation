1161756c9f27911ceb194743432742599331ab32
Alumno <Alumno@ALUMNO-PC>
2019-02-26 09:07:28 +0000

    Empezando el Informe

--- src/main/java/main/launcher/uso/InformeFactory.java
+++ src/main/java/main/launcher/uso/InformeFactory.java
@@ -5,16 +5,36 @@ import java.util.ArrayList;
 
 import launcher.puntuaciones.Puntuacion;
 import launcher.puntuaciones.Puntuaciones;
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRField;
 
-public class InformeFactory {
+public class InformeFactory implements JRDataSource{
 
 	Puntuaciones puntuaciones;
+	private int indice = -1;
+	private ArrayList<Puntuacion> puntos;
 	public ArrayList<Puntuacion> load() throws IOException{
 		puntuaciones=new Puntuaciones();
 		puntuaciones.cargarPuntuaciones();
-		ArrayList<Puntuacion> puntos=new ArrayList<>();
+		puntos=new ArrayList<>();
 		puntos.addAll(puntuaciones.listaInforme());
 		return puntos;
+	}
+	
+	@Override
+	public boolean next() throws JRException {
+		return ++indice < puntos.size();
+	}
+	@Override
+	public Object getFieldValue(JRField jrField) throws JRException {
+		Object valor = null;
+		if("nombre".equals(jrField.getName())) {
+			valor = puntos.get(indice).getNombre();
+		}else if("puntos".equals(jrField.getName())) {
+			valor = puntos.get(indice).getPuntos();
+		}
+		return valor;
 	}
 	
 }
